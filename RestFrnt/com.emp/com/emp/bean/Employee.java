95d5e764be326e8ef0235e97788493160f4c020c
Jovan <Jovan@DESKTOP-7KRBJ60>
2018-05-22 17:04:17 +0200

    jovan 22-05-2018

--- RestFrnt/com.emp/com/emp/bean/Employee.java
+++ RestFrnt/com.emp/com/emp/bean/Employee.java
@@ -2,39 +2,20 @@ package com.emp.bean;
 
 import java.io.Serializable;
 
-import javax.persistence.Column; 
-import javax.persistence.Entity; 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table; 
-
-
 /**
  * @author Jovan
  *
  */
-@Entity
-@Table(name="employee")
-@NamedQuery(name = "findAllEmployees", query = "SELECT e FROM Employee e")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+
 public class Employee implements Serializable  {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id")
+
 	private Long id;
-	@Column(name="name")
 	private String name;
-	@Column(name="jobDescription")
 	private String jobDescription;
-	@Column(name="monthSalary")
 	private Double monthSalary;
 	
 	public Employee() {
